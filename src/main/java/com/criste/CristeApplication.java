bce4fe3cd43ded48c7f4142fa0889e43287e8108
EssieDyva <st3f1703@gmail.com>
2025-09-17 11:09:07 +0200

    spring boot setup

---
+++ src/main/java/com/criste/CristeApplication.java
@@ -0,0 +1,13 @@
+package com.criste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CristeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CristeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/criste/CristeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.criste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CristeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
