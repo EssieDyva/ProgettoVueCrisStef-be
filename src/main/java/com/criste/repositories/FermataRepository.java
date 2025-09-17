504468412f91bc2e6ca742b06b9a88bca82b3c9a
EssieDyva <st3f1703@gmail.com>
2025-09-17 16:42:12 +0200

    JpaRepository instead of CrudRepository

--- src/main/java/com/criste/repositories/FermataRepository.java
+++ src/main/java/com/criste/repositories/FermataRepository.java
@@ -1,11 +1,11 @@
 package com.criste.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.criste.models.Fermata;
 
 @Repository
-public interface FermataRepository extends CrudRepository<Fermata, Long> {
+public interface FermataRepository extends JpaRepository<Fermata, Long> {
     
 }
